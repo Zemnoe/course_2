5fc89cec0be0199b37c13201ec98adbbf65a4538
Zemnoe <nzemnoe@gmail.com>
2023-07-29 21:55:33 +0100

    index.py file

---
+++ index.py
@@ -0,0 +1,40 @@
+first_name = "Paschalina"
+middle_name = "Zemnoe"
+last_name = "Manomi"
+print(f"my first name and middle name is {first_name} {middle_name} while my surname is {last_name}")
+
+first_name = "Paschalina"
+print(type(first_name))
+
+X = 1.2
+print(type (X))
+
+Z = 15
+print(type(Z))
+
+students = ["Ada", "Bola", "Gayle"]
+print(type (students))
+
+A = 2j
+print(type (A))
+
+fruits = ("pineapple", "Coconut", "Mango")
+print(type (fruits))
+
+points = range (10)
+print(type (points))
+
+name_surname = {"Mark":"John", "Lina":"Joy", "Salma":"Thomas"}
+print(type(name_surname))
+
+K = {"Achievers"}
+print (type (K))
+
+y = ({"Drake", "Nike"})
+print (type(y))
+
+greetings = b"hello"
+print (type (greetings))
+
+Good_day = True
+print(type(Good_day))
